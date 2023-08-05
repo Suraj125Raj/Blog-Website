c3328064a806ec49162f505faef3a68798045e00
Suraj Singh <137762948+Suraj125Raj@users.noreply.github.com>
2023-08-05 15:13:03 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const sessionSecret = "blogsessionusersecret";
 
-const emailUser = "sunghsuraj06@gmail.com";
-const emailPassword ="Suraj#125";
+const emailUser = "";
+const emailPassword ="";
 
 module.exports ={
     sessionSecret,
